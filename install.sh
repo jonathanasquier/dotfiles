7a036e577c9ac6d955898196b1ee672e7b825570
Jonathan Asquier <jonathan.asquier@gmail.com>
2016-11-16 18:51:12 +0100

    Initial commit

---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo apt-get install compton
+sudo apt-get install i3lock
+sudo apt-get install scrot
+sudo apt-get install nitrogen
+sudo apt-get install sysstat
+sudo apt-get install acpi
+sudo apt-get install rofi
+sudo apt-get install fonts-font-awesome
+sudo apt-get install xbacklight
+
+gsettings set org.gnome.desktop.background show-desktop-icons false
+sudo apt-get purge dunst
+
