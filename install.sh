1743f16399a82185133074f6315dea60ec85cd3f
Jonathan Asquier <jonathan.asquier@gmail.com>
2016-11-28 16:58:33 +0100

    Add software to install

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # install apps
-sudo apt-get install compton i3 i3lock scrot nitrogen sysstat acpi rofi fonts-font-awesome xbacklight ranger chromium-browser redshift
+sudo apt-get install compton i3 i3lock scrot nitrogen sysstat acpi rofi fonts-font-awesome xbacklight ranger chromium-browser redshift gimp git
 
 # launch and configure nitrogen for background images
 
