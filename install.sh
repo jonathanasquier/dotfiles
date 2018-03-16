1e5c97808f6372807bc4b56199062dd4b3dd4f46
Jonathan Asquier <jonathan.asquier@gmail.com>
2018-03-16 16:15:20 +0100

    Updates

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 # install apps
-sudo apt-get install lxappearance compton i3 i3lock i3blocks i3status suckless-tools scrot nitrogen sysstat acpi rofi fonts-font-awesome xbacklight ranger chromium-browser redshift gimp git zsh
-
+sudo apt-get install lxappearance compton i3 i3lock i3blocks i3status suckless-tools scrot nitrogen sysstat acpi rofi fonts-font-awesome xbacklight ranger chromium-browser redshift gimp git zsh git-flow
 # LOG TO I3
 
 # use lxappearance to change to font size of GTK software
@@ -15,4 +14,9 @@ gsettings set org.gnome.desktop.background show-desktop-icons false
 # back to nice unity notifications
 sudo apt-get purge dunst
 
-# install docker, atom, ulauncher, ctop (on github)
+# install docker, atom, ulauncher, ctop (on github), postman, mongodb compass, fzf https://github.com/junegunn/fzf
+
+# install spaceship theme for zsh https://github.com/denysdovhan/spaceship-prompt
+# patch ~/.oh-my-zsh/custom/themes/spaceship-prompt/sections/dir.zsh with
+# " ENV=[${ENV}] " \
+# in spaceship::section to have a dynamic env displayed in the prompt
